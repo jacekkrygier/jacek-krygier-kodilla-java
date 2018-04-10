73290749ddc251b742af9f9506dfa32dcf42b777
jacekkrygier <jacekkrygier1705@gmail.com>
2018-04-10 10:25:46 +0100

    Poprawka 7.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderServiceApplication.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderServiceApplication.java
@@ -6,9 +6,9 @@ public class ProductOrderServiceApplication {
         Customer jacekkrygier = new Customer("Jacek", "Krygier","jacekkrygier1705");
         Product ps4 = new Product("PlayStation 4", 799.99, 1);
         OrderRequest orderRequest = new OrderRequest(jacekkrygier, ps4, "FS33728278");
-        InformationServiceClass informationService = new InformationServiceClass();
-        OrderRepositoryClass orderRepository = new OrderRepositoryClass();
-        OrderServiceClass orderService = new OrderServiceClass();
+        InformationService informationService = new InformationServiceClass();
+        OrderRepository orderRepository = new OrderRepositoryClass();
+        OrderService orderService = new OrderServiceClass();
 
         ProductOrderService productOrderService = new ProductOrderService(informationService, orderService, orderRepository);
         productOrderService.process(orderRequest);
