cef751567578237dd8fe5fe5363e146f008622f9
jacekkrygier <jacekkrygier1705@gmail.com>
2018-04-14 10:49:53 +0100

    Modul 10.2

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Display.java
@@ -4,4 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Display {
+    public void displayValue(double val) {
+        System.out.println("Result: " + val);
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -1,12 +1,32 @@
 package com.kodilla.spring.calculator;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootTest
 public class CalculatorTestSuite {
     @Test
     public void testCalculations() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        double a = 7.5;
+        double b =3.75;
 
+        //When
+        double addResult = calculator.add(a, b);
+        double subResult = calculator.sub(a, b);
+        double mulResult = calculator.mul(a, b);
+        double divResult = calculator.div(a, b);
+
+        //Then
+        Assert.assertEquals(11.25, addResult, 0);
+        Assert.assertEquals(3.75, subResult, 0);
+        Assert.assertEquals(28.125, mulResult, 0);
+        Assert.assertEquals(2, divResult, 0);
     }
 }
