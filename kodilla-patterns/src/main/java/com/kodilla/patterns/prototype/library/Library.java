8f9d8ba563f34fb299c3adfb880f44fba2dd81fb
jacekkrygier <jacekkrygier1705@gmail.com>
2018-04-22 10:24:04 +0100

    Zadanie 11.2 poprawka

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -33,7 +33,8 @@ public final class Library extends Prototype {
         Library clonedLibrary = (Library) super.clone();
         clonedLibrary.books = new HashSet<>();
         for (Book book : books) {
-            clonedLibrary.getBooks().add(book);
+            Book clonedBook = new Book(book.getTitle(), book.getAuthor(), book.getPublicationDate());
+            clonedLibrary.getBooks().add(clonedBook);
         }
         return clonedLibrary;
     }
