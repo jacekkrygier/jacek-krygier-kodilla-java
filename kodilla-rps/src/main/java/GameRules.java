3bed021112ff8c377c3c40fab536d2a50d5ac40e
jacekkrygier <jacekkrygier1705@gmail.com>
2018-05-03 20:43:58 +0100

    Zadanie 13.1

--- kodilla-rps/src/main/java/GameRules.java
+++ kodilla-rps/src/main/java/GameRules.java
@@ -31,16 +31,37 @@ public class GameRules {
     public String getChoice() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Select: R for Rock, S for Scissors or P for Paper");
-            String choice = scanner.nextLine().toUpperCase();
+            System.out.println("Select: 1 for Rock, 2 for Paper, 3 for Scissors, x for end of the game, n for the new game");
+            String choice = scanner.nextLine();
             switch (choice) {
-                case "R": return "Rock";
-                case "S": return "Scissors";
-                case "P": return "Paper";
+                case "1": return "Rock";
+                case "2": return "Paper";
+                case "3": return "Scissors";
+                case "x": return ending();
             }
             System.out.println("Wrong selection");
         }
     }
+
+    public String ending() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("You want to quit the game. Are you sure? Select 1 for Yes or 2 for No");
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "1":
+                    return "Yes";
+                case "2":
+                    return "No";
+            }
+            System.out.println("Wrong selection");
+        }
+
+
+
+    }
+
+
 }
 
 
